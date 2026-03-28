fed537fb0f6cdae0e83ad2b0ad989189405f3277
marionmorel <marion.mrl@live.fr>
2026-03-28 22:07:39 +0000

    Tasks 1 - 6

--- script.py
+++ script.py
@@ -1,3 +1,27 @@
 import pandas as pd
 
 ad_clicks = pd.read_csv('ad_clicks.csv')
+
+# Task 1
+print(ad_clicks.head(10))
+
+# Task 2
+views_by_source = ad_clicks.groupby('utm_source').user_id.count().reset_index()
+print(views_by_source)
+
+# Task 3
+ad_clicks['is_click'] = ~ad_clicks.ad_click_timestamp.isnull()
+
+# Task 4
+clicks_by_source = ad_clicks.groupby(['utm_source', 'is_click']).user_id.count().reset_index()
+
+# Task 5
+clicks_pivot = clicks_by_source.pivot(
+    columns='is_click',
+    index='utm_source',
+    values='user_id'
+).reset_index()
+
+# Task 6
+clicks_pivot['percent_clicked'] = clicks_pivot[True] / (clicks_pivot[True] + clicks_pivot[False]) 
+print(clicks_pivot)
